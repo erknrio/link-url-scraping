d0668420d2cda74406faabdbedc4d6d7f21e288d
erknrio <erknrio@gmail.com>
2016-01-10 21:45:53 +0000

    La url acepta parametro maximo iteraciones
    Se han corregido varios problemas con la recursividad que no imprimian adecuadamente las url.

--- scraping.py
+++ scraping.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
+import re
 from bs4 import BeautifulSoup
 import requests
-import re
-reload(sys)
-sys.setdefaultencoding('UTF8')
 
 try:
     def valid_url(url_string):
@@ -16,53 +14,53 @@ try:
         else:
             return True
     
-    def scrapingURL(url, num=-1, recursive = False):
+    def scrapingURL(url, iteraciones = 0, saltos = 0):
+        i = 0
+        j = 0
+        whitespaces = '    '
         # Realizamos la peticion
         req = requests.get(url)
-        statusCode = req.status_code
-        
-        # Peticion es correcta
-        if statusCode == 200:
+        status_code = req.status_code
+        if status_code == 200:
             # Pasamos el contenido HTML de la web a un objeto BeautifulSoup()
             html = BeautifulSoup(req.text, "html.parser")
             # Obtenemos todos los enlaces
             enlaces = html.find_all('a',{'href' : True})
-            # Recorremos todos los enlaces para extraer al atributo href
+            # Recorremos todos los enlaces para extraer los atributos href
             for i,enlace in enumerate(enlaces):
+                # url absolutas
                 if valid_url(enlace["href"]):
-                    print "---", enlace["href"]
-        else:
-            print "Status Code: ", statusCode
+                    print u"" + (whitespaces * saltos) + "---" +  enlace["href"]
+                    iteraciones -= 1
+                    if iteraciones > 0:
+                        scrapingURL(enlace["href"], iteraciones, saltos + 1)
+        elif status_code == 404:
+            print u"Url inalcanzable"
 
     def main():
+        iteraciones = 0
         num_params = len(sys.argv)
-        param3 = ''
 
         if num_params < 2:
             raise IndexError("Debe introducir una URL")
         elif num_params > 3:
             raise IndexError("Exceso de parámetros")
-        elif num_params == 3:
-            param3 = int(sys.argv[2])
-            if not isinstance(param3, (int) ):
+        else:
+            if num_params == 3:
+                iteraciones = int(sys.argv[2])
+            if not isinstance(iteraciones, (int) ):
                 raise TypeError("El último argumento debe ser númerico")
             else:
-                if callable(scrapingURL) and callable(valid_url):
+                if callable(valid_url) and callable(scrapingURL):
                     if valid_url(sys.argv[1]):
-                        scrapingURL(sys.argv[1], param3)
+                        scrapingURL(sys.argv[1], iteraciones)
                     else:
                         raise Exception("URL no válida")
                 else:
-                    print "El método no existe"
-        else:
-            if callable(scrapingURL) and callable(valid_url):
-                valid_url(sys.argv[1])
-                scrapingURL(sys.argv[1])
-            else:
-                print "El método no existe"
+                    raise Exception("No se puede ejecutar")
         
     if __name__=="__main__":
         main()
             
 except Exception as err:
-        print "Ha ocurrido un error: ", err
+        print u"Ha ocurrido un error: ", err
